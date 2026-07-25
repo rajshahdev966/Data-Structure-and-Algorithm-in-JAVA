b1fb2e610c153dd324ea13ebe079b38a4680c727
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 12:21:53 +0530

    Updated portfolio content

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -1,6 +1,20 @@
-package Patterns;
-java.util.Scanner;
+
+import java.util.Scanner;
+
+// package Patterns;
 
 public class patterns {
-    
+    static void pattern1(){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i <= num; i++) {
+            for (int j = 0; j <= num; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern1();
+    }
 }
