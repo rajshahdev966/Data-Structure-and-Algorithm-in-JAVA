dea39a6a939a6e99471d66a76360dc43def6d055
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 20:24:29 +0530

    Updated portfolio content

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -243,12 +243,30 @@ public class patterns {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for (int i = num; i >= 1; i--) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for(int k = 0; k< )
+            for(int k = 1; k<= 2 * (num - i); k++ ){
+                System.out.print(" ");
+            }
+            for (int m = 1; m <= i; m++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for(int k = 1; k<= 2 * (num - i); k++ ){
+                System.out.print(" ");
+            }
+            for (int m = 1; m <= i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
