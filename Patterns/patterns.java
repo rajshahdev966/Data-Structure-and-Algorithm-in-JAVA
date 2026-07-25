de30c8ce45a62d072fb8cfa62c89da7047fe03f3
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 12:27:40 +0530

    Update project files

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -24,8 +24,19 @@ public class patterns {
             System.out.println();
         }
     }
+    static void pattern3(){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // pattern1();
-        pattern2();
+        // pattern2();
+        pattern3();
     }
 }
