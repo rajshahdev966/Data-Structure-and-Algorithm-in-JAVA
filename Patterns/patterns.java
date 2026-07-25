75f38d44382b8392a2f8963f44df42d702196fe2
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 22:09:18 +0530

    Updated portfolio content

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -326,11 +326,14 @@ public class patterns {
         int num = sc.nextInt();
         for (int i = 1; i <= (2 * num) - 1; i++) {
             for (int j = 1; j <= (2 * num) - 1; j++) {
-                if (i == 1 || i == (2 * num) - 1 || j == 1 || j == (2 * num) - 1) {
-                    System.out.print(num);
+                if ((i >= 1 && i <= 5) && (j >= 1 && j <= 5)) {
+                    System.out.print(3);
                 }
-                if ((i == 2 || i == (2 * num) - 2) && (j == 2 || j == (2 * num) - 2)) {
-                    System.out.print(num - 1);
+                if ((i >= 2 && i <= 4) && (j >= 2 && i <= 4)) {
+                    System.out.print(2);
+                }
+                if ((i >= 3 && i <= 3) && (j >= 3 && i <= 3)) {
+                    System.out.print(1);
                 }
                
 
