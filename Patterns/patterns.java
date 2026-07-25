77074c13a77cbe89e861239a354794ce11b0e24f
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 12:28:51 +0530

    Auto sync changes

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -34,9 +34,20 @@ public class patterns {
             System.out.println();
         }
     }
+    static void pattern4(){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // pattern1();
         // pattern2();
-        pattern3();
+        // pattern3();
+        pattern4();
     }
 }
