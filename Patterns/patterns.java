184f2cd751afaf08e05f39238dc8096fbb2f9b19
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 22:36:26 +0530

    Modified website assets

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -324,8 +324,22 @@ public class patterns {
     static void pattern22() {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        // for (int i = 1; i <= (2 * num) - 1; i++) {
+        //     for (int j = 1; j <= (2 * num) - 1; j++) {
+        //         if ((i == 1 || i == 5) || (j == 1 || j == 5)) {
+        //             System.out.print("3 ");
+        //         }
+        //         else if ((i == 2 || i == 4) || (j == 2 || j == 4)) {
+        //             System.out.print("2 ");
+        //         }
+        //         else if ((i == 3 || i == 3) || (j == 3 || j == 3)) {
+        //             System.out.print("1 ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
         for (int i = 1; i <= (2 * num) - 1; i++) {
-            for (int j = 1; j <= (2 * num) - 1; j++) {
+            for (int j = 0; j < (2 * num) - 1; j++) {
                 if ((i == 1 || i == 5) || (j == 1 || j == 5)) {
                     System.out.print("3 ");
                 }
