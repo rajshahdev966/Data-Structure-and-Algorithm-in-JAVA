99bfd7d778efc40d7e8aa88cc29a2f90bd2ad38e
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 22:50:01 +0530

    Update project files

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -340,16 +340,16 @@ public class patterns {
         // }
         for (int i = 1; i <= (2 * num) - 1; i++) {
             for (int j = 1; j <= (2 * num) - 1; j++) {
-                if ((i == 1 || i == 8) || (j == 1 || j == 8)) {
+                if ((i == 1 || i == 9) || (j == 1 || j == 9)) {
                     System.out.print("5 ");
                 }
-                else if ((i == 2 || i == 7) || (j == 2 || j == 7)) {
+                else if ((i == 2 || i == 8) || (j == 2 || j == 8)) {
                     System.out.print("4 ");
                 }
-                else if ((i == 3 || i == 6) || (j == 3 || j == 6)) {
+                else if ((i == 3 || i == 7) || (j == 3 || j == 7)) {
                     System.out.print("3 ");
                 }
-                else if ((i == 4 || i == 5) || (j == 4 || j == 5)) {
+                else if ((i == 4 || i == 6) || (j == 4 || j == 6)) {
                     System.out.print("2 ");
                 }
                 else if ((i == 5 || i == 5) || (j == 5 || j == 5)) {
