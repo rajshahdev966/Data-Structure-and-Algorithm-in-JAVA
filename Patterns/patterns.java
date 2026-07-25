d0661107608c5ade5292e76f31595d8b3ab53004
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 12:43:27 +0530

    Updated portfolio content

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -71,7 +71,7 @@ public class patterns {
             for (int j = 1; j <= num - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= ((2 * i) - 1); k += 2) {
+            for (int k = 1; k <= ((2 * i) - 1); k ++) {
                 System.out.print("*");
             }
 
