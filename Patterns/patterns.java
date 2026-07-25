a06867c30a8c4f6d181a64af20757f101358e10f
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 18:04:35 +0530

    Modified website assets

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -226,6 +226,17 @@ public class patterns {
             
         }
     }
+    static void pattern18() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        char chToPrint = 'A';
+        for (int i = 1; i <= num; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                
+            }
+        }
+    }
 
     public static void main(String[] args) {
         // pattern1();
