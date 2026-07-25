2c391bbf52a30a695f60a6881899d3ded82df912
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-25 20:58:23 +0530

    Auto sync changes

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -317,26 +317,26 @@ public class patterns {
     }
 
     public static void main(String[] args) {
-        // pattern1();
-        // pattern2();
-        // pattern3();
-        // pattern4();
-        // pattern5();
-        // pattern6();
-        // pattern7();
-        // pattern8();
-        // pattern9();
-        // pattern10();
-        // pattern11();
-        // pattern12();
-        // pattern13();
-        // pattern14();
-        // pattern15();
-        // pattern16();
-        // pattern17();
-        // pattern18();
-        // pattern19();
-        // pattern20();
+        pattern1();
+        pattern2();
+        pattern3();
+        pattern4();
+        pattern5();
+        pattern6();
+        pattern7();
+        pattern8();
+        pattern9();
+        pattern10();
+        pattern11();
+        pattern12();
+        pattern13();
+        pattern14();
+        pattern15();
+        pattern16();
+        pattern17();
+        pattern18();
+        pattern19();
+        pattern20();
         pattern21();
         // pattern22();
     }
