e52eed83d463ad6c62bf93bd1c9a2dc199497590
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-26 09:08:59 +0530

    Updated portfolio content

--- Array Basics/arrayBasics.java
+++ Array Basics/arrayBasics.java
@@ -1,23 +1,43 @@
 
-
 public class arrayBasics {
-    static void sumOfElement(int[] arr){
+
+    static void sumOfElement(int[] arr) {
         int sum = 0;
-       for (int i = 0; i < arr.length; i++) {
-           sum += arr[i];
-       }
-       System.out.println(sum);
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
     }
 
+    static int countOdd(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    static boolean sortedCheck(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-
-
-
-
-
+    static void reverseArr(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - i - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[arr.length - i - 1];
+                arr[arr.length - i - 1] = temp;
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        sumOfElement([1,2,3,4,5,6,7]);
     }
 }
