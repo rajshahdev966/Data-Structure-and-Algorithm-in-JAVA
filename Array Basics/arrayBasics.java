06f28bb3746b2534634abe9f0d116fdce85d7ed9
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-26 08:21:52 +0530

    Modified website assets

--- Array Basics/arrayBasics.java
+++ Array Basics/arrayBasics.java
@@ -1,7 +1,23 @@
+
+import java.util.Scanner;
+
 // package Array Basics;
 
 public class arrayBasics {
+    static void sumOfElement(){
+        Scanner sc = new Scanner(System.im);
+        int[] arr = sc.
+    }
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        
+        System.out.println("HELLO");
     }
 }
